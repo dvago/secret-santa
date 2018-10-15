fc20bd5c1f840b51e14f7127be430a8de6eb1ce0
Davide Vago <davide.vago@zpg.co.uk>
2018-10-15 13:27:29 +0100

    Add extra check for current user + disable button once clicked

--- src/components/Selection.vue
+++ src/components/Selection.vue
@@ -1,11 +1,12 @@
 <template>
   <main>
+    My current user: {{ currentName }}
     <ul>
       <li v-for="(item, index) in list" :key="index">
         {{ item }}
       </li>
     </ul>
-    <button @click="randomPick">pick your santa</button>
+    <button @click="randomPick" :disabled="list.length <= 0 || alreadySelected">pick your santa</button>
     {{ myPick }}
     <button @click="logout">See you at the party!</button>
   </main>
@@ -22,12 +23,15 @@ export default {
     return {
       db: Firebase.database(),
       list: null,
-      myPick: null
+      myPick: null,
+      alreadySelected: false,
+      currentName: Firebase.auth().currentUser.email
     }
   },
   created () {
+    this.currentName = this.cleanUpUserName()
     this.db.ref('users/').once('value', (snapshot) => {
-      this.list = snapshot.val()
+      this.list = this.removeCurrentUserFromList(snapshot.val())
     })
   },
   methods: {
@@ -36,15 +40,22 @@ export default {
         this.$router.replace('/login')
       })
     },
+    cleanUpUserName () {
+      return this.currentName.slice(0, this.currentName.indexOf('@')).toLowerCase()
+    },
+    removeCurrentUserFromList (list) {
+      return list.filter(item => item !== this.currentName)
+    },
     randomPick () {
-      let index
       const selection = Math.floor(Math.random() * this.list.length)
 
       this.myPick = this.list[selection]
-      index = this.list.indexOf(this.myPick)
-      this.list.splice(index, 1)
-
+      this.list = this.list.filter(item => item !== this.myPick)
       this.db.ref('users/').set(this.list)
+      this.disablePickButton()
+    },
+    disablePickButton () {
+      this.alreadySelected = true
     }
   }
 }
